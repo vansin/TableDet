bd31addd17eb61d9273c6d32f470a311101f94e5
vansin <msnode@163.com>
2022-06-03 11:21:16 +0800

    feat: update auto_scale_lr

--- configs/icdar2019_tracka_250/yolox/yolox_s_8x8_300e_coco.py
+++ configs/icdar2019_tracka_250/yolox/yolox_s_8x8_300e_coco.py
@@ -162,4 +162,4 @@ log_config = dict(interval=50)
 # NOTE: `auto_scale_lr` is for automatically scaling LR,
 # USER SHOULD NOT CHANGE ITS VALUES.
 # base_batch_size = (8 GPUs) x (8 samples per GPU)
-auto_scale_lr = dict(base_batch_size=64)
+# auto_scale_lr = dict(base_batch_size=64)
