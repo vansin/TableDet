1c0c6d588136e5301c0f8e77858457459eb54300
vansin <msnode@163.com>
2022-06-05 10:47:42 +0800

    prepare_datasets_automl

---
+++ tools/prepare_datasets_automl.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+
+mkdir /root/autodl-tmp/datasets
+mkdir /root/autodl-tmp/vansin_work_dirs
+
+
+ln -s /root/autodl-tmp/datasets  $(pwd)/data
+ln -s /root/autodl-tmp/vansin_work_dirs $(pwd)/work_dirs
+
