81bb8375f4f0f054ccc98472c9e5d4215f55dd43
vansin <msnode@163.com>
2022-06-12 14:11:06 +0800

    feat: update

--- configs/icdar2019_tracka_250_1gpu/_base_/schedules/schedule_CosineRestartLr_1x.py
+++ configs/icdar2019_tracka_250_1gpu/_base_/schedules/schedule_CosineRestartLr_1x.py
@@ -3,5 +3,5 @@ optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
 # learning policy
 lr_config = dict(
-    policy='CosineRestart')
+    policy='CosineRestart', periods=0.01)
 runner = dict(type='EpochBasedRunner', max_epochs=12)
--- configs/icdar2019_tracka_250_1gpu/_base_/schedules/schedule_PolyLr_1x.py
+++ configs/icdar2019_tracka_250_1gpu/_base_/schedules/schedule_PolyLr_1x.py
@@ -3,5 +3,5 @@ optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
 # learning policy
 lr_config = dict(
-    policy='ExpLr')
+    policy='Poly')
 runner = dict(type='EpochBasedRunner', max_epochs=12)
