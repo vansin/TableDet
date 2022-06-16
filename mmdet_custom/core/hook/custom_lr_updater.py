1f6c91ab33a0482b8df8ba24eab646f8c8d2ce08
vansin <msnode@163.com>
2022-06-16 11:58:55 +0800

    feat: update

--- mmdet_custom/core/hook/custom_lr_updater.py
+++ mmdet_custom/core/hook/custom_lr_updater.py
@@ -51,3 +51,51 @@ class CustomLrUpdaterHook(LrUpdaterHook):
             else:
                 warmup_lr = self.get_warmup_lr(cur_iter)
                 self._set_lr(runner, warmup_lr)
+
+@HOOKS.register_module()
+class OnlineLrUpdaterHook(LrUpdaterHook):
+
+    def __init__(self, gamma=0.8, **kwargs):
+        self.gamma = gamma
+        super(CustomLrUpdaterHook, self).__init__(**kwargs)
+
+    def get_lr(self, runner, base_lr):
+        progress = runner.epoch if self.by_epoch else runner.iter
+        return base_lr * self.gamma**progress
+
+    def before_train_epoch(self, runner):
+        # overwrite the before_train_epoch
+        return
+
+    def before_train_iter(self, runner):
+        # overwrite the before_train_epoch
+        return
+
+    def after_train_epoch(self, runner):
+        if self.warmup_iters is None:
+            epoch_len = len(runner.data_loader)
+            self.warmup_iters = self.warmup_epochs * epoch_len
+
+        if not self.by_epoch:
+            return
+
+        self.regular_lr = self.get_regular_lr(runner)
+        self._set_lr(runner, self.regular_lr)
+
+    def after_train_iter(self, runner):
+        cur_iter = runner.iter
+        if not self.by_epoch:
+            self.regular_lr = self.get_regular_lr(runner)
+            if self.warmup is None or cur_iter >= self.warmup_iters:
+                self._set_lr(runner, self.regular_lr)
+            else:
+                warmup_lr = self.get_warmup_lr(cur_iter)
+                self._set_lr(runner, warmup_lr)
+        elif self.by_epoch:
+            if self.warmup is None or cur_iter > self.warmup_iters:
+                return
+            elif cur_iter == self.warmup_iters:
+                self._set_lr(runner, self.regular_lr)
+            else:
+                warmup_lr = self.get_warmup_lr(cur_iter)
+                self._set_lr(runner, warmup_lr)
