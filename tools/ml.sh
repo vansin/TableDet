2a25734a5fac49ed63c597f3e91449afcd42f187
vansin <msnode@163.com>
2022-07-04 18:07:58 +0800

    feat: update

--- tools/ml.sh
+++ tools/ml.sh
@@ -1,8 +1,12 @@
 mkdir /root/autodl-tmp/datasets
 mkdir /root/autodl-tmp/vansin_work_dirs
 
-mv /root/autodl-tmp/icdar2019_tracka_modern.zip /root/autodl-tmp/datasets/
+wget https://cdn.vansin.top/OpenMMLab/icdar2019_tracka_modern.zip -O /root/autodl-tmp/icdar2019_tracka_modern.zip
+
+# mv /root/autodl-tmp/icdar2019_tracka_modern.zip /root/autodl-tmp/datasets/
 
 cd /root/autodl-tmp/datasets/
 
-upzip icdar2019_tracka_modern.zip
+
+
+unzip icdar2019_tracka_modern.zip
