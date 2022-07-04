e15a4d55758e58164ae0ad6479454c5a81c67df6
vansin <msnode@163.com>
2022-07-04 17:45:57 +0800

    feat: ml.sh

---
+++ tools/ml.sh
@@ -0,0 +1,8 @@
+mkdir /root/autodl-tmp/datasets
+mkdir /root/autodl-tmp/vansin_work_dirs
+
+mv /root/autodl-tmp/icdar2019_tracka_modern.zip /root/autodl-tmp/datasets/
+
+cd /root/autodl-tmp/datasets/
+
+upzip icdar2019_tracka_modern.zip
