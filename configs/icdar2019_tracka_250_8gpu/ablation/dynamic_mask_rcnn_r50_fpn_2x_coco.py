b41d864839fbd26d36ee64bd6d3c23ec58a87f84
vansin <msnode@163.com>
2022-07-04 15:57:09 +0800

    feat: update

---
+++ configs/icdar2019_tracka_250_8gpu/ablation/dynamic_mask_rcnn_r50_fpn_2x_coco.py
@@ -0,0 +1,34 @@
+_base_ = [
+    '../_base_/models/mask_rcnn_r50_fpn.py',
+    '../_base_/datasets/coco_instance.py',
+    '../_base_/schedules/schedule_2x.py', '../_base_/default_runtime.py'
+]
+
+
+model = dict(
+    roi_head=dict(
+        type='DynamicRoIHead',
+        bbox_head=dict(
+            type='Shared2FCBBoxHead',
+            in_channels=256,
+            fc_out_channels=1024,
+            roi_feat_size=7,
+            num_classes=1,
+            bbox_coder=dict(
+                type='DeltaXYWHBBoxCoder',
+                target_means=[0., 0., 0., 0.],
+                target_stds=[0.1, 0.1, 0.2, 0.2]),
+            reg_class_agnostic=False,
+            loss_cls=dict(
+                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
+            loss_bbox=dict(type='SmoothL1Loss', beta=1.0, loss_weight=1.0))),
+    train_cfg=dict(
+        rpn_proposal=dict(nms=dict(iou_threshold=0.85)),
+        rcnn=dict(
+            dynamic_rcnn=dict(
+                iou_topk=75,
+                beta_topk=10,
+                update_iter_interval=100,
+                initial_iou=0.4,
+                initial_beta=1.0))),
+    test_cfg=dict(rpn=dict(nms=dict(iou_threshold=0.85))))
