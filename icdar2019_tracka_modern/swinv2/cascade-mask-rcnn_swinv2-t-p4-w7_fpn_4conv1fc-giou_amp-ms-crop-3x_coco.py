a7f1d9a7f6ccdfaca464e8c4087d6e45e211dc7b
vansin <msnode@163.com>
2022-10-05 14:26:45 +0800

    [Feature] update param_scheduler

--- icdar2019_tracka_modern/swinv2/cascade-mask-rcnn_swinv2-t-p4-w7_fpn_4conv1fc-giou_amp-ms-crop-3x_coco.py
+++ icdar2019_tracka_modern/swinv2/cascade-mask-rcnn_swinv2-t-p4-w7_fpn_4conv1fc-giou_amp-ms-crop-3x_coco.py
@@ -125,18 +125,50 @@ train_dataloader = dict(dataset=dict(pipeline=train_pipeline))
 max_epochs = 36
 train_cfg = dict(max_epochs=max_epochs)
 
-# learning rate
+# # learning rate
+# param_scheduler = [
+#     dict(
+#         type='LinearLR', start_factor=0.001, by_epoch=False, begin=0,
+#         end=1000),
+#     dict(
+#         type='MultiStepLR',
+#         begin=0,
+#         end=max_epochs,
+#         by_epoch=True,
+#         milestones=[27, 33],
+#         gamma=0.1)
+# ]
+
+
+optim_wrapper = dict(
+    optimizer=dict(
+        _delete_=True,
+        type='AdamW',
+        lr=5e-4 * 1024 / 512,
+        weight_decay=0.05,
+        eps=1e-8,
+        betas=(0.9, 0.999)),
+    paramwise_cfg=dict(
+        norm_decay_mult=0.0,
+        bias_decay_mult=0.0,
+        custom_keys={
+            '.absolute_pos_embed': dict(decay_mult=0.0),
+            '.relative_position_bias_table': dict(decay_mult=0.0)
+        }),
+)
+
+# learning policy
 param_scheduler = [
+    # warm up learning rate scheduler
     dict(
-        type='LinearLR', start_factor=0.001, by_epoch=False, begin=0,
-        end=1000),
-    dict(
-        type='MultiStepLR',
-        begin=0,
-        end=max_epochs,
+        type='LinearLR',
+        start_factor=1e-3,
         by_epoch=True,
-        milestones=[27, 33],
-        gamma=0.1)
+        end=20,
+        # update by iter
+        convert_to_iter_based=True),
+    # main learning rate scheduler
+    dict(type='CosineAnnealingLR', eta_min=1e-5, by_epoch=True, begin=20)
 ]
 
 # Enable automatic-mixed-precision training with AmpOptimWrapper.
