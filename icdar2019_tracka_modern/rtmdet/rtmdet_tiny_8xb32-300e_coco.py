78358ba9f369e7f65bb9c919457ab7d1c4c22dc2
vansin <msnode@163.com>
2022-10-05 21:15:22 +0800

    update config

--- icdar2019_tracka_modern/rtmdet/rtmdet_tiny_8xb32-300e_coco.py
+++ icdar2019_tracka_modern/rtmdet/rtmdet_tiny_8xb32-300e_coco.py
@@ -1,13 +1,14 @@
 _base_ = './rtmdet_s_8xb32-300e_coco.py'
 
-checkpoint = 'TODO:imagenet_pretrain'  # noqa
+# checkpoint = 'TODO:imagenet_pretrain'  # noqa
 
 model = dict(
     backbone=dict(
         deepen_factor=0.167,
         widen_factor=0.375,
-        init_cfg=dict(
-            type='Pretrained', prefix='backbone.', checkpoint=checkpoint)),
+        # init_cfg=dict(
+        #     type='Pretrained', prefix='backbone.', checkpoint=checkpoint)
+        ),
     neck=dict(in_channels=[96, 192, 384], out_channels=96, num_csp_blocks=1),
     bbox_head=dict(in_channels=96, feat_channels=96, exp_on_reg=False))
 
