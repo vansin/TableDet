326a502d93092f1e15c263793237b37ec306674f
vansin <msnode@163.com>
2022-10-06 14:09:19 +0800

    Feat: add  cd /project/mmlab2/mmdet-best-practices ; /usr/bin/env /home/elaine/miniconda3/envs/mmyd/bin/python /home/elaine/.vscode/extensions/ms-python.python-2022.14.0/pythonFiles/lib/python/debugpy/adapter/../../debugpy/launcher 46807 -- tools/analysis_tools/get_flops.py /project/mmlab2/configs/icdar2019_tracka_modern/cascade_rcnn/cascade-mask-rcnn_r50_fpn_1x_coco.py

---
+++ icdar2019_tracka_modern/_base_/datasets/coco_instance_boundary_m.py
@@ -0,0 +1,103 @@
+# dataset settings
+dataset_type = 'CocoDataset'
+data_root = 'data/icdar2019_tracka_modern/'
+
+# file_client_args = dict(
+#     backend='petrel',
+#     path_mapping=dict({
+#         './data/': 's3://openmmlab/datasets/detection/',
+#         'data/': 's3://openmmlab/datasets/detection/'
+#     }))
+
+metainfo = {
+    'CLASSES': ('table', ),
+    'PALETTE': [
+        (220, 20, 60),
+    ]
+}
+
+file_client_args = dict(backend='disk')
+
+train_pipeline = [
+    dict(type='LoadImageFromFile', file_client_args=file_client_args),
+    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
+    dict(type='Resize', scale=(1333, 800), keep_ratio=True),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PackDetInputs')
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile', file_client_args=file_client_args),
+    dict(type='Resize', scale=(1333, 800), keep_ratio=True),
+    # If you don't have a gt annotation, delete the pipeline
+    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
+    dict(
+        type='PackDetInputs',
+        meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape',
+                   'scale_factor'))
+]
+
+train_dataloader = dict(
+    batch_size=2,
+    num_workers=4,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    batch_sampler=dict(type='AspectRatioBatchSampler'),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        metainfo=metainfo,
+        ann_file='train_m.json',
+        data_prefix=dict(img='train_img/'),
+        filter_cfg=dict(filter_empty_gt=True, min_size=32),
+        pipeline=train_pipeline))
+val_dataloader = dict(
+    batch_size=1,
+    num_workers=2,
+    persistent_workers=True,
+    drop_last=False,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        metainfo=metainfo,
+        ann_file='test.json',
+        data_prefix=dict(img='test_img/'),
+        test_mode=True,
+        pipeline=test_pipeline))
+test_dataloader = val_dataloader
+
+# val_evaluator = dict(
+#     type='CocoMetric',
+#     ann_file=data_root + 'val.json',
+#     metric=['bbox', 'segm'],
+#     format_only=False)
+
+val_evaluator = dict(
+    type='F1Metric',
+    ann_file=data_root + 'test.json',
+    metric='mAP',
+    format_only=False)
+test_evaluator = val_evaluator
+
+
+# inference on test dataset and
+# format the output results for submission.
+# test_dataloader = dict(
+#     batch_size=1,
+#     num_workers=2,
+#     persistent_workers=True,
+#     drop_last=False,
+#     sampler=dict(type='DefaultSampler', shuffle=False),
+#     dataset=dict(
+#         type=dataset_type,
+#         data_root=data_root,
+#         ann_file=data_root + 'annotations/image_info_test-dev2017.json',
+#         data_prefix=dict(img='test2017/'),
+#         test_mode=True,
+#         pipeline=test_pipeline))
+# test_evaluator = dict(
+#     type='CocoMetric',
+#     metric=['bbox', 'segm'],
+#     format_only=True,
+#     ann_file=data_root + 'annotations/image_info_test-dev2017.json',
+#     outfile_prefix='./work_dirs/coco_instance/test')
