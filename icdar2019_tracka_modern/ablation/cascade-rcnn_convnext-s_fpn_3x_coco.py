10f0bd37102c485a8931439fdb0440b907a093ed
vansin <msnode@163.com>
2022-10-07 00:55:05 +0800

    [Feature] update

--- icdar2019_tracka_modern/ablation/cascade-rcnn_convnext-s_fpn_3x_coco.py
+++ icdar2019_tracka_modern/ablation/cascade-rcnn_convnext-s_fpn_3x_coco.py
@@ -20,3 +20,36 @@ model = dict(
             prefix='backbone.')),
         neck=dict(in_channels=[96, 192, 384, 768])
     )
+
+max_epochs = 36
+train_cfg = dict(max_epochs=max_epochs)
+
+# learning rate
+param_scheduler = [
+    dict(
+        type='LinearLR', start_factor=0.001, by_epoch=False, begin=0,
+        end=1000),
+    dict(
+        type='MultiStepLR',
+        begin=0,
+        end=max_epochs,
+        by_epoch=True,
+        milestones=[27, 33],
+        gamma=0.1)
+]
+
+# Enable automatic-mixed-precision training with AmpOptimWrapper.
+optim_wrapper = dict(
+    type='AmpOptimWrapper',
+    constructor='LearningRateDecayOptimizerConstructor',
+    paramwise_cfg={
+        'decay_rate': 0.7,
+        'decay_type': 'layer_wise',
+        'num_layers': 12
+    },
+    optimizer=dict(
+        _delete_=True,
+        type='AdamW',
+        lr=0.0002,
+        betas=(0.9, 0.999),
+        weight_decay=0.05))
